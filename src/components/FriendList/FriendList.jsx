24721b1a9b5c5471db06e0a1593e18737d78ea13
Унжакова Екатерина <katerina.unz@gmail.com>
2021-10-25 04:07:06 +0700

    Стили friendslist

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
+import s from './FriendList.module.css'
 
 
 const FriendList =({friends}) =>  (
-        <ul className="friend-list">
+        <ul className={s.FriendList}>
             {friends.map(friend =>
-                (<li className="item" key={friend.id}>
-                <span className="status" style={{backgroundColor: friend.isOnline ? "green": "red"}} ></span>
-                <img className="avatar" src={friend.avatar } alt={friend.name } width="48" />
-                <p className="name">{friend.name }</p>
+                (<li className={s.item} key={friend.id}>
+                <span className={s.status} style={{backgroundColor: friend.isOnline ? "green": "red"}} ></span>
+                <img className={s.avatar} src={friend.avatar } alt={friend.name } width="48" />
+                <p className={s.name}>{friend.name }</p>
             </li>))}
         </ul>);
     
